1a23fa10700db36653df63d7e275400b0ba5d633
Mayra Lucero Garcia Ramírez <ml17ft@gmail.com>
2022-04-19 23:18:59 -0500

    Section 1: Add db context and migrations

---
+++ DapperDemo/DapperDemo/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using DapperDemo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DapperDemo.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<Company> Companies { get; set; }
+    }
+
+}
